b051d8e29095b11ad1902400f7275f8c73e0005d
Matthew Cummings <zombymatthew@gmail.com>
2016-07-29 18:13:48 -0700

    Added a couple more things

--- src/main/java/com/zombymatthew/flipper/App.java
+++ src/main/java/com/zombymatthew/flipper/App.java
@@ -1,14 +1,42 @@
 package com.zombymatthew.flipper;
 
+import java.nio.file.Path;
+
 public class App 
 {
-  public static void main( String[] args ) throws Exception
+  protected Path watchPath = null;
+  public static void main (String [] args) throws Exception
   {
 
 
     Flipper flip = new Flipper (null);
   }
  
+  
+  public void parseArguments (String [] args)
+  {
+    for (String argument: args)
+    {
+      if (argument.startsWith ("-"))
+      {
+        if (argument.contains ("="))
+        {
+          
+        }
+        else
+        {
+          
+        }
+
+      }
+      else 
+      {
+        System.err.println ("Unknow argument: " + argument);
+      }
+    }
+    
+  }
+  
   
 
 }
